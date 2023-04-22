5c19eea258da05cbdf579d1fe053f4d9dda978f1
Gustavo Nascimento <gs-nascimento@outlook.com>
2023-04-21 22:16:43 -0300

    fix(card): image load bug fixed

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -6,16 +6,19 @@ import Section from '../Section'
 import Pokemon from '../../shared/types/pokemon'
 
 import notFoundImage from '../../assets/image_not_found.png'
+import CardSkeleton from '../CardSkeleton'
 
 interface CardProps {
   item: Promise<Pokemon>
 }
 const Card: React.FC<CardProps> = ({ item }) => {
   const [isLoading, setIsLoading] = useState(true)
-
+  const [imageDidLoad, setImageDidLoad] = useState(false)
   const [pokemon, setPokemon] = useState({} as Pokemon)
 
   const loadPokemonData = useCallback(async () => {
+    setIsLoading(true)
+
     const loadedPokemon = await item
     setPokemon(loadedPokemon)
 
@@ -28,41 +31,46 @@ const Card: React.FC<CardProps> = ({ item }) => {
 
   return (
     <>
-      {!isLoading && (
-        <div id="card-container">
-          <div id="card-image-section">
-            <img
-              id="card-image-content"
-              src={pokemon.image ?? notFoundImage}
-              style={
-                !pokemon.image
-                  ? { width: 'auto' }
-                  : {}}
-              alt={!pokemon.image
-                ? 'not_found'
-                : 'pokemon'}
-            />
-          </div>
-          <div id="card-info">
-            <p id="card-item-name">{pokemon.name.toUpperCase()}</p>
-            <Section title="Stats">
-              {pokemon.stats.map(stat => (
-                <div key={`${stat.name}_${stat.value}`} id="card-stat-item">
-                  <p>{stat.name}</p>
-                  <p>{stat.value}</p>
-                </div>
-              ))}
-            </Section>
-            <Section title="Types">
-              <div id="card-badge-container">
-                {pokemon.types.map(type => (
-                  <Badge key={type} type={type} />
-                ))}
+      <div id="card-container">
+        {!isLoading && imageDidLoad
+          ? (
+            <>
+
+              <div id="card-image-section">
+                <>
+                  <img
+                    id="card-image-content"
+                    src={pokemon.image ?? notFoundImage}
+                    style={{ visibility: imageDidLoad ? 'visible' : 'hidden' }}
+                    alt={!pokemon.image
+                      ? 'not_found'
+                      : 'pokemon'}
+                    onLoad={() => { setImageDidLoad(true) }}
+                  />
+                </>
               </div>
-            </Section>
-          </div>
-        </div>
-      )}
+              <div id="card-info">
+                <p id="card-item-name">{pokemon.name.toUpperCase()}</p>
+                <Section title="Stats">
+                  {pokemon.stats.map(stat => (
+                    <div key={`${stat.name}_${stat.value}`} id="card-stat-item">
+                      <p>{stat.name}</p>
+                      <p>{stat.value}</p>
+                    </div>
+                  ))}
+                </Section>
+                <Section title="Types">
+                  <div id="card-badge-container">
+                    {pokemon.types.map(type => (
+                      <Badge key={type} type={type} />
+                    ))}
+                  </div>
+                </Section>
+              </div>
+            </>)
+          : (<CardSkeleton />)
+        }
+      </div>
     </>
   )
 }
